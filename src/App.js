0061d31b018b1b14508f123bca3a7ac362ea01f8
jito <jitokj3@gmail.com>
2020-11-18 14:00:57 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Login from "./Login";
+
 
 function App() {
   return (
     <div className="app">
-      Spotify Clone
+      <Login />
     </div>
   );
 }
