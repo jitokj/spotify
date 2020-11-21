dc0249e378f770e2312f4b0d1fc3e709ee199daf
jito <jitokj3@gmail.com>
2020-11-21 16:38:33 +0530

    added sidebaroptions

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./SideBar.css";
+import SideBarOption from './SideBarOption';
 
 const SideBar = () => {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+                <img className="sidebr__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+         alt="spotify logo"/>
+         <SideBarOption title="Home" />
+         <SideBarOption title="Search" />
+         <SideBarOption title="Your Library" />
         </div>
     );
 };
