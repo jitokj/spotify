7fe5139244ca13dc5a8b8a332640d53b28609fef
jito <jitokj3@gmail.com>
2020-11-21 23:09:47 +0530

    added material-ui icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./Footer.css";
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <h1>Footer</h1>
+          <div className="footer__left">
+              <p>Album and song</p>
+          </div>
+          <div className="footer__center">
+             <ShuffleIcon className="footer__green" />
+             <SkipPreviousIcon className="footer__icon" />
+             <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
+             <SkipNextIcon className="footer__icon" />
+             <RepeatIcon className="footer__green" />
+          </div>
+          <div className="footer__right">
+              Volume Controls
+          </div>
         </div>
     );
 };
