5f02b5a7d4aae05a37556e230eaf9b5ab8fb15bf
jito <jitokj3@gmail.com>
2020-11-21 15:05:11 +0530

    added context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import {useStateValue} from "./StateProvider"
 
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  //const [token,setToken] = useState(null);
+  const [{user,token},dispatch] = useStateValue();
+
+  useEffect(()=>{
+    const hash = getTokenFromUrl();
+
+  
+   
+    const _token = hash.access_token;
+   
+    if(_token) {
+     dispatch({
+       type: "SET_TOKEN",
+       token: _token
+     })
+      spotify.setAccessToken(_token);
+      spotify.getMe()
+      .then(user=>{
+      
+        dispatch({
+          type: "SET_USER",
+          user: user
+        })
+      })
+    }
+   
+    window.location.hash = "";
+  },[]);
+
+  console.log("👦",user);
   return (
     <div className="app">
-      <Login />
+
+    {
+      token ? (<Player />): (<Login />)
+    }
     </div>
   );
 }
