0ec1bf49fd5e0b5e340bd9e82643dac1e00e46c2
jito <jitokj3@gmail.com>
2020-11-22 11:08:29 +0530

    styled footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,19 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
 
 const Footer = () => {
     return (
         <div className="footer">
           <div className="footer__left">
-              <p>Album and song</p>
+           <img className="footer__albumLogo" src="https://upload.wikimedia.org/wikipedia/commons/4/45/Usher.jpg" alt=""/>
+           <div className="footer__songInfo">
+               <h4>Yeah!</h4>
+               <p>Usher</p>
+           </div>
           </div>
           <div className="footer__center">
              <ShuffleIcon className="footer__green" />
@@ -20,7 +27,17 @@ const Footer = () => {
              <RepeatIcon className="footer__green" />
           </div>
           <div className="footer__right">
-              Volume Controls
+             <Grid container spacing={2}>
+                 <Grid item >
+                     <PlaylistPlayIcon />
+                 </Grid>
+                 <Grid item >
+                     <VolumeDownIcon />
+                 </Grid>
+                 <Grid item xs >
+                     <Slider />
+                 </Grid>
+             </Grid>
           </div>
         </div>
     );
